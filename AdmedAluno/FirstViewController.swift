ac59202004d5ea37569c592b7ffc5e2e5d16f695
ramondev <andraderamondev@gmail.com>
2018-06-10 22:00:06 -0300

    Correções de layouts.

--- AdmedAluno/FirstViewController.swift
+++ AdmedAluno/FirstViewController.swift
@@ -8,16 +8,40 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UITableViewController {
+    var cursos: [String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.contentInset = UIEdgeInsetsMake(0, -10, 0, 0);
+        self.cursos.append("Enxaqueca 1")
+        self.cursos.append("Enxaqueca 2")
+        self.cursos.append("Enxaqueca 3")
+        self.cursos.append("Enxaqueca 4")
+        self.cursos.append("Enxaqueca 5")
+        
     }
-
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.cursos.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let rowUse = "rowUse"
+        let cel = tableView.dequeueReusableCell(withIdentifier: rowUse, for: indexPath) as! CursoCell
+        let t = self.cursos[indexPath.row]
+        cel.cursoTitulo.text = t
+        //cel.textLabel?.text = filme.getTitle()
+        //cel.imageView?.image = filme.getImage()
+        return cel
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
 
 }
 
